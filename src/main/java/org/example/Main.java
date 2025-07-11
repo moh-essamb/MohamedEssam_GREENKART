14a343e40a4a106775bf95f2a7e6b256682972c2
moh-essamb <moh.essamb@gmail.com>
2025-07-11 04:08:20 +0300

    m2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Main {
 
@@ -17,7 +15,7 @@ public class Main {
     protected static WebDriverWait waitDriver;
 
     // List of products and their quantities
-    protected static String[] products = {"Cucumber","Carrot","Tomato", "Mushroom"};
+    protected static String[] products = {"mixture","Cucumber","  ","Carrot","hh","Tomato","Mushro","ghgh"};
     protected static String promoCode="test";
 
     // WebElements
@@ -32,46 +30,58 @@ public class Main {
 
     //Loops over a list of products to add each to the cart.
     public void addMultipleProductsToCart(String[] products) {
-        for (int i = 0; i < products.length; i++) {
-            try {
-                // Locate the Add to Cart button using product name
-                WebElement addToCartButton = driver.findElement(By.xpath("//h4[contains(text(), '" + products[i] + "')]/parent::div/div[@class='product-action']/button"));
-                addToCartButton.click(); // Click the Add to Cart button
-            } catch (NoSuchElementException e) {
-                System.out.println("Product: " + products[i] + " is not found");
-                // Optionally continue or log more details
+        List <WebElement> productsList = driver.findElements(By.cssSelector(".product-name"));
+        boolean productIsFound;
+        for (int j=0 ; j<products.length ; j++){
+            productIsFound=false;
+            if((products[j].length()<3)||(products[j].isBlank())){
+                System.out.printf("Product: (%s) is invalid\n",products[j]);
+                continue;
+            }
+            for(int i=0 ; i<productsList.size() ; i++){
+                String productName = productsList.get(i).getText().toLowerCase();
+                if(productName.contains(products[j].toLowerCase())){
+                    ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0);");
+                    addToCartButton = driver.findElement(By.xpath(String.format("(//div[@class='product-action'])[%d]//button",i+1)));
+                    waitDriver.until(ExpectedConditions.elementToBeClickable(addToCartButton)).click();
+                    productIsFound=true;
+                    break;
+                }
+            }
+            if (!productIsFound){
+                System.out.printf("Product: (%s) is not found\n",products[j]);
             }
         }
-     }
-
-    public void checkOut(String promoCode){
-        cartIcon = driver.findElement(By.xpath("//img[@alt='Cart']"));
-        cartIcon.click();
-
-        checkOutBtn = driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']"));
-        checkOutBtn.click();
-
-        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".promoCode")));
-
-        promoCodeTbox = driver.findElement(By.cssSelector(".promoCode"));
-        promoCodeTbox.sendKeys(promoCode);
-
-        promoBtn = driver.findElement(By.cssSelector(".promoBtn"));
-        promoBtn.click();
-
-        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".promoInfo")));
-
-        promoInfo = driver.findElement(By.cssSelector(".promoInfo"));
-        System.out.println("Code information message: " + promoInfo.getText());
-
-        placeOrderBtn = driver.findElement(By.xpath("//button[text()='Place Order']"));
-        placeOrderBtn.click();
-
     }
+
+//    public void checkOut(String promoCode){
+//        cartIcon = driver.findElement(By.xpath("//img[@alt='Cart']"));
+//        cartIcon.click();
+//
+//        checkOutBtn = driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']"));
+//        checkOutBtn.click();
+//
+//        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".promoCode")));
+//
+//        promoCodeTbox = driver.findElement(By.cssSelector(".promoCode"));
+//        promoCodeTbox.sendKeys(promoCode);
+//
+//        promoBtn = driver.findElement(By.cssSelector(".promoBtn"));
+//        promoBtn.click();
+//
+//        waitDriver.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".promoInfo")));
+//
+//        promoInfo = driver.findElement(By.cssSelector(".promoInfo"));
+//        System.out.println("Code information message: " + promoInfo.getText());
+//
+//        placeOrderBtn = driver.findElement(By.xpath("//button[text()='Place Order']"));
+//        placeOrderBtn.click();
+//
+//    }
 
     public static void main(String[] args) {
         Main m = new Main();
         m.addMultipleProductsToCart(products);
-        m.checkOut(promoCode);
+       // m.checkOut(promoCode);
     }
 }
